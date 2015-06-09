5284f1b36a56e64b39f4580379ec2307f5d96511
Chase Lewis <chasedlewis@gmail.com>
2015-06-08 21:19:36 -0400

    Created Bio App
    Played with seques

---
+++ Classwork/Lesson 1/Homework 2.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let currentTimeAndDate = NSDate()
+var currentTemperature = 72.0
+println("Current temperature oustide is \(currentTemperature)")
+
+let oneHourFromNow = NSDate(timeIntervalSinceNow: 3600)
+currentTemperature = 74.0
+println("Current temperature oustide is \(currentTemperature)")
+
+let twoHoursFromNow = NSDate(timeIntervalSinceNow: 6400)
+currentTemperature = 76.0
+println("Current temperature oustide is \(currentTemperature)")
+
+let threeHoursFromNow = NSDate(timeIntervalSinceNow: 9600)
+currentTemperature = 80.0
+println("Current temperature oustide is \(currentTemperature)")
