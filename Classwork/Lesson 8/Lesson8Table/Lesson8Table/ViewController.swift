2b91c6a081892db90048af0cd8973c0218d66b3c
Chase Lewis <chasedlewis@gmail.com>
2015-06-30 16:14:48 -0400

    Completed Homework #2 and created Lesson 8 notes

---
+++ Classwork/Lesson 8/Lesson8Table/Lesson8Table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson8Table
+//
+//  Created by Chase Lewis on 6/29/15.
+//  Copyright (c) 2015 Chase Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Classwork/Lesson 8/Lesson8Table/TableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TableViewController.swift
+//  Lesson8Table
+//
+//  Created by Chase Lewis on 6/29/15.
+//  Copyright (c) 2015 Chase Lewis. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
